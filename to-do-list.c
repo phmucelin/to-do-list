cdffc00639e5a34b4d0241975b612d3980593967
Pedro Mucelin <contatopedromucelin05@gmail.com>
2025-08-28 10:13:02 -0300

    Update to-do-list.c
    add new options

--- to-do-list.c
+++ to-do-list.c
@@ -34,7 +34,7 @@ void addItemList(){
     totalItens++;
 }
 
-void saveItemInList(){
+void saveANDload(){
     fptr = fopen("list.txt", "w");
     if(fptr == NULL){
         printf("Sua lista esta vazia, voce deve adicionar itens antes de executar!");
@@ -54,22 +54,70 @@ void removeItemList(){
     /*Pesquisar como remover item de uma lista vindo de um struct*/
 }
 
+void check(){
+    ptr_items = &list[totalItens];
+    fptr = open("list.txt", "w");
+    if(fptr==NULL){
+        printf("Error! Voce nao possui itens em sua lista, por favor adicione!");
+        saveItemInList();
+    }
+    char filterByName[100];
+    printf("Qual o nome da tarefa que voce deseja marcar como concluida? ");
+    scanf(" %[^\n]", filterByName);
+    if(filterByName == items.itemName){
+        int UserConfirmation = 0;
+        printf("Irei alterar a tarefa para CONCLUIDA, voce confirma? 1 para SIM 0 para NAO: ");
+        scanf("%d", &UserConfirmation);
+        switch (UserConfirmation)
+        {
+        case 1:
+            items.done = 1;
+            break;
+        case 0:
+            printf("Finalizando a aplicacao, nenhum dado foi alterado!");
+            break;
+        default:
+            printf("ERROR! Voce esta inserindo valores que nao correspondem a nenhuma opcao!");
+            break;
+        }
+    }
+}
+
 void readList(){
+    int escolha = 0;
     fptr = fopen("list.txt", "r");
 
     if(fptr == NULL){
         printf("Voce deve inserir elementos em sua lista antes de lista-los.\n");
         return;
     }
-    if(list->done == NULL){
-        printf("Os itens que nao foram marcados como feitos, sao: \n");
-        printf("%s",list->itemName);
+    printf("Voce deseja filtrar a sua pesquisa? 0 SIM 1 NAO: ");
+    scanf("%d", &escolha);
+    if(escolha==1){
+        int concluedorno = 0;
+        printf("Voce deseja filtrar por CONCLUIDAS ou por NAO CONCLUIDAS: 0 CONCLUIDAS 1 NAO CONCLUIDAS");
+        scanf("%d", &concluedorno);
+        switch (concluedorno)
+        {
+        case 1:
+            if(list->done == NULL){
+            printf("Os itens que nao foram marcados como feitos, sao: \n");
+            printf("%s",list->itemName);
+            break;
+        case 2:
+            printf("Itens concluidos: \n");
+            printf("%s", list->itemName);
+            break;
+        default:
+            printf("Voce esta inserindo um valor invalido.");
+            break;
+        }
+            
+        }
     }else{
-        printf("Itens concluidos: \n");
-        printf("%s", list->itemName);
-    }
-    while(fgets(MAX_ITENS, sizeof(MAX_ITENS), fptr)){
-        printf("%s", MAX_ITENS);
+        while(fgets(MAX_ITENS, sizeof(MAX_ITENS), fptr)){
+            printf("%s", MAX_ITENS);
+        }
     }
     fclose(fptr);
 
